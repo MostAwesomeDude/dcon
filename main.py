034b93df5f36722fc48eea71ab8a9d0930be6f56
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-06-18 22:06:10 -0700

    Add upload form, templates. Uploads work.

--- main.py
+++ main.py
@@ -5,16 +5,26 @@ if __name__ == "__main__":
     raise SystemExit(main.app.run(debug=True))
 
 from datetime import datetime
+import os.path
 
 from sqlalchemy.orm.exc import NoResultFound
 
-from flask import Flask, abort, redirect, url_for
+from werkzeug import secure_filename
+
+from flask import Flask, abort, redirect, render_template, url_for
 from flaskext.sqlalchemy import SQLAlchemy
+from flaskext.uploads import configure_uploads, IMAGES, UploadSet
+from flaskext.wtf import Form, FileField, file_allowed, file_required
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///temp.db"
+app.config["SECRET_KEY"] = "just a test!"
+app.config["UPLOADS_DEFAULT_DEST"] = "uploads"
 
 db = SQLAlchemy(app)
+images = UploadSet("images", IMAGES)
+
+configure_uploads(app, (images,))
 
 class Comic(db.Model):
     """
@@ -47,12 +57,30 @@ class Comic(db.Model):
     def __repr__(self):
         return "<Comic(%r)>" % self.filename
 
+class UploadForm(Form):
+    file = FileField("Upload",
+        validators=(file_required(), file_allowed(images, "Images only!")))
+
+@app.route("/upload", methods=("GET", "POST"))
+def upload():
+    form = UploadForm()
+
+    if form.validate_on_submit():
+        filename = secure_filename(form.file.file.filename)
+        path = os.path.abspath(os.path.join("uploads", filename))
+        if not os.path.exists(path):
+            form.file.file.save(path)
+
+        return redirect(url_for("index"))
+
+    return render_template("upload.html", form=form)
+
 @app.errorhandler(404)
 def not_found(error):
     return "Couldn't find the page!", 404
 
 @app.route("/")
-def root():
+def index():
     return "One second, please."
 
 @app.route("/comics/")
