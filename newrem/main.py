ff7a6384704d26d300dde988f82faaa5514def63
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-06-22 18:20:57 -0700

    Move templates and make things work again.

--- newrem/main.py
+++ newrem/main.py
@@ -1,6 +1,10 @@
+import os
+
 from flask import Flask
 
+wd = os.getcwd()
+
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///temp.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s/temp.db" % wd
 app.config["SECRET_KEY"] = "just a test!"
-app.config["UPLOADS_DEFAULT_DEST"] = "uploads"
+app.config["UPLOADS_DEFAULT_DEST"] = os.path.join(wd, "uploads")
