43562ee28833cbd697638a0c1e222ef94add22bc
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-07-07 03:42:45 -0700

    main: Display SQL for now.
    Helps see hotspots.

--- newrem/main.py
+++ newrem/main.py
@@ -7,6 +7,7 @@ wd = os.getcwd()
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s/temp.db" % wd
+app.config["SQLALCHEMY_ECHO"] = True
 app.config["SECRET_KEY"] = "just a test!"
 app.config["UPLOADS_DEFAULT_DEST"] = os.path.join(wd, "uploads")
 
