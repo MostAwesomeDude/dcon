056d483d68c67ddcfab5db5fb467b3723ddb14ca
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-07-24 12:48:33 -0700

    Add users blueprint.

--- newrem/main.py
+++ newrem/main.py
@@ -3,6 +3,9 @@ import os
 from flask import Flask
 from flaskext.login import LoginManager
 
+from newrem.comics import comics
+from newrem.users import users
+
 wd = os.getcwd()
 
 app = Flask(__name__)
@@ -13,3 +16,6 @@ app.config["UPLOADS_DEFAULT_DEST"] = os.path.join(wd, "uploads")
 
 lm = LoginManager()
 lm.setup_app(app)
+
+app.register_blueprint(users)
+app.register_blueprint(comics)
