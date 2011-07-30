1606f7dd7f2a44b67259a583c8c0f556524a389d
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-07-29 23:28:32 -0700

    main: Permit use with Twisted Web.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
+from newrem.main import app
+from newrem import views
+
 if __name__ == "__main__":
-    from newrem.main import app
-    from newrem import views
     raise SystemExit(app.run(debug=True, host="0.0.0.0"))
