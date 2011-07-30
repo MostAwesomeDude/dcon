cc72af47031f416a5dcc28b51eb5930a99d01bd8
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-07-30 11:59:22 -0700

    Add a shell.
    $ python -i -mshell

---
+++ shell.py
@@ -0,0 +1,8 @@
+import readline, rlcompleter
+readline.parse_and_bind("tab:complete")
+
+from newrem.main import app
+from newrem.models import *
+
+db.init_app(app)
+app.test_request_context().push()
