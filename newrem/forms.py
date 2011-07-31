56c1fefac37a88d22d9ff227d9464aeae645d6ff
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-07-31 11:23:38 -0700

    forms: Fix import.

--- newrem/forms.py
+++ newrem/forms.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 
 from wtforms.ext.dateutil.fields import DateTimeField
+from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from wtforms.fields import Field
 from wtforms.validators import EqualTo
 from wtforms.widgets import TextInput
 
 from flaskext.uploads import IMAGES, UploadSet
 from flaskext.wtf import (Form, FileAllowed, FileRequired,
-    Required, FileField, QuerySelectField, IntegerField,
-    PasswordField, SubmitField, TextField, TextAreaField)
+    Required, FileField, IntegerField, PasswordField, SubmitField, TextField,
+    TextAreaField)
 
 from newrem.models import Character, Portrait
 
