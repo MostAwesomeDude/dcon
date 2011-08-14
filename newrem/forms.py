c821c7db98ff0959cc0930182cbffe71069766c6
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-08-14 00:42:28 -0700

    forms: Add fields and validators to UploadForm.

--- newrem/forms.py
+++ newrem/forms.py
@@ -2,14 +2,13 @@ from datetime import datetime
 
 from wtforms.ext.dateutil.fields import DateTimeField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.fields import Field
-from wtforms.validators import EqualTo
+from wtforms.fields import Field, TextAreaField
+from wtforms.validators import EqualTo, Length
 from wtforms.widgets import TextInput
 
 from flaskext.uploads import IMAGES, UploadSet
 from flaskext.wtf import (Form, FileAllowed, FileRequired,
-    Required, FileField, IntegerField, PasswordField, SubmitField, TextField,
-    TextAreaField)
+    Required, FileField, IntegerField, PasswordField, SubmitField, TextField)
 
 from newrem.models import Character, Portrait
 
@@ -88,7 +87,9 @@ class UploadForm(Form):
     file = FileField("Select a file to upload",
         validators=(FileRequired("Must upload a comic!"),
             FileAllowed(images, "Images only!")))
-    title = TextField("Title", validators=(Required(),))
+    title = TextField("Title", validators=(Required(), Length(max=80)))
+    description = TextAreaField("Alternate Text")
+    comment = TextAreaField("Commentary")
     index = IntegerField("Where to insert this comic?",
         validators=(Required(),))
     characters = TagListField("Characters")
