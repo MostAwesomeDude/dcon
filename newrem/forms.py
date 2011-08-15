40a73022ac545db4134b69a35e43776fcea29fd3
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-08-14 23:08:39 -0700

    forms: Switch upload form to a real query field.

--- newrem/forms.py
+++ newrem/forms.py
@@ -1,10 +1,10 @@
 from datetime import datetime
 
 from wtforms.ext.dateutil.fields import DateTimeField
-from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.fields import Field, TextAreaField
+from wtforms.ext.sqlalchemy.fields import (QuerySelectMultipleField,
+    QuerySelectField)
+from wtforms.fields import TextAreaField
 from wtforms.validators import EqualTo, Length
-from wtforms.widgets import TextInput
 
 from flaskext.uploads import IMAGES, UploadSet
 from flaskext.wtf import (Form, FileAllowed, FileRequired,
@@ -14,22 +14,6 @@ from newrem.models import Character, Portrait
 
 images = UploadSet("images", IMAGES)
 pngs = UploadSet("pngs", ("png",))
-
-class TagListField(Field):
-    widget = TextInput()
-
-    def _value(self):
-        if self.data:
-            return u", ".join(self.data)
-        else:
-            return u""
-
-    def process_formdata(self, data):
-        if data:
-            self.data = [word.strip() for word in data[0].split(",")]
-            self.data.sort()
-        else:
-            self.data = []
 
 portrait = FileField("Select a portrait",
     validators=(FileAllowed(pngs, "PNGs only!"),))
@@ -92,6 +76,8 @@ class UploadForm(Form):
     comment = TextAreaField("Commentary")
     index = IntegerField("Where to insert this comic?",
         validators=(Required(),))
-    characters = TagListField("Characters")
+    characters = QuerySelectMultipleField(u"Characters",
+        query_factory=lambda: Character.query.order_by(Character.name),
+        get_label="name")
     time = DateTimeField("Activation time", default=datetime.now())
     submit = SubmitField("Upload!")
