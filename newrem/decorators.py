7d4157b0cdac597fc050f4762a09a83697075221
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-08-20 19:08:03 -0700

    decorators: Nuke make_auth_required().

--- newrem/decorators.py
+++ newrem/decorators.py
@@ -4,33 +4,7 @@ from werkzeug.contrib.cache import SimpleCache
 
 from flask import request
 
-from newrem.security import Authenticator
-
 cache = SimpleCache()
-
-def make_auth_required(app):
-    path = app.config["DCON_PASSWORD_FILE"]
-    d = {}
-    for line in open(path, "rb").read().split("\n"):
-        try:
-            k, v = line.split(":", 1)
-        except ValueError:
-            pass
-        else:
-            d[k.strip()] = v.strip()
-
-    authenticator = Authenticator(d)
-
-    def auth_required(f):
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            auth = request.authorization
-            if not auth or not authenticator.validate(auth):
-                return authenticator.make_basic_challenge("Cid's Lair",
-                    "Haha, no.")
-            return f(*args, **kwargs)
-        return decorated
-    return auth_required
 
 def cached(f):
     @wraps(f)
