04efe0bc75fa7665448b59157c88b77d09585148
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-09-02 03:55:56 -0700

    grammars: Make full paragraph formatting work.

--- newrem/grammars.py
+++ newrem/grammars.py
@@ -5,6 +5,9 @@ blog_grammar = """
 doublecrlf ::= '\r' '\n' '\r' '\n' => "</p><p>"
 
 crlf ::= '\r' '\n' => "<br />"
+
+paragraphs ::= (<doublecrlf> | <crlf> | <anything>)*:l
+    => "<p>%s</p>" % "".join(l)
 """
 
 class BlogGrammar(OMeta.makeGrammar(blog_grammar, globals())):
--- newrem/test/test_grammars.py
+++ newrem/test/test_grammars.py
@@ -10,3 +10,20 @@ class TestBlogGrammar(TestCase):
     def test_doublecrlf(self):
         self.assertEqual(BlogGrammar("\r\n\r\n").apply("doublecrlf")[0],
             "</p><p>")
+
+    def test_paragraphs_empty(self):
+        self.assertEqual(BlogGrammar("").apply("paragraphs")[0], "<p></p>")
+
+    def test_paragraphs_trivial(self):
+        self.assertEqual(BlogGrammar("asdf").apply("paragraphs")[0],
+            "<p>asdf</p>")
+
+    def test_paragraphs_break(self):
+        text = "asdf\r\njkl"
+        self.assertEqual(BlogGrammar(text).apply("paragraphs")[0],
+            "<p>asdf<br />jkl</p>")
+
+    def test_paragraphs_multiple(self):
+        text = "asdf\r\n\r\njkl"
+        self.assertEqual(BlogGrammar(text).apply("paragraphs")[0],
+            "<p>asdf</p><p>jkl</p>")
