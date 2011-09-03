fd8e787dcd5b78688ffd50fc24d70e67aa552140
Corbin Simpson <MostAwesomeDude@gmail.com>
2011-09-03 01:10:22 -0700

    grammars: BIU.

--- newrem/grammars.py
+++ newrem/grammars.py
@@ -6,8 +6,21 @@ doublecrlf ::= '\r' '\n' '\r' '\n' => "</p><p>"
 
 crlf ::= '\r' '\n' => "<br />"
 
-paragraphs ::= (<doublecrlf> | <crlf> | <anything>)*:l
+not_crlf ::= ~'\r' <anything>
+
+double_star ::= '*' '*'
+
+bold ::= <double_star> (~<double_star> <not_crlf>)+:b <double_star>
+    => "<b>%s</b>" % "".join(b)
+
+italics ::= '*' (~'*' <not_crlf>)+:i '*' => "<i>%s</i>" % "".join(i)
+
+underline ::= '_' (~'_' <not_crlf>)+:u '_' => "<u>%s</u>" % "".join(u)
+
+paragraphs ::=
+    (<doublecrlf> | <crlf> | <bold> | <italics> | <underline> | <anything>)*:l
     => "<p>%s</p>" % "".join(l)
+
 """
 
 class BlogGrammar(OMeta.makeGrammar(blog_grammar, globals())):
--- newrem/test/test_grammars.py
+++ newrem/test/test_grammars.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 
+from pymeta.runtime import ParseError
+
 from newrem.grammars import BlogGrammar
 
 class TestBlogGrammar(TestCase):
@@ -10,6 +12,24 @@ class TestBlogGrammar(TestCase):
     def test_doublecrlf(self):
         self.assertEqual(BlogGrammar("\r\n\r\n").apply("doublecrlf")[0],
             "</p><p>")
+
+    def test_not_crlf(self):
+        self.assertEqual(BlogGrammar("a").apply("not_crlf")[0], "a")
+
+    def test_not_crlf_fail(self):
+        self.assertRaises(ParseError, BlogGrammar("\r").apply, "not_crlf")
+
+    def test_bold(self):
+        self.assertEqual(BlogGrammar("**asdf**").apply("bold")[0],
+            "<b>asdf</b>")
+
+    def test_italics(self):
+        self.assertEqual(BlogGrammar("*asdf*").apply("italics")[0],
+            "<i>asdf</i>")
+
+    def test_underline(self):
+        self.assertEqual(BlogGrammar("_asdf_").apply("underline")[0],
+            "<u>asdf</u>")
 
     def test_paragraphs_empty(self):
         self.assertEqual(BlogGrammar("").apply("paragraphs")[0], "<p></p>")
@@ -27,3 +47,13 @@ class TestBlogGrammar(TestCase):
         text = "asdf\r\n\r\njkl"
         self.assertEqual(BlogGrammar(text).apply("paragraphs")[0],
             "<p>asdf</p><p>jkl</p>")
+
+    def test_paragraphs_italics(self):
+        text = "*asdf*"
+        self.assertEqual(BlogGrammar(text).apply("paragraphs")[0],
+            "<p><i>asdf</i></p>")
+
+    def test_paragraphs_italics_cross(self):
+        text = "*as\r\n\r\ndf*"
+        self.assertEqual(BlogGrammar(text).apply("paragraphs")[0],
+            "<p>*as</p><p>df*</p>")
