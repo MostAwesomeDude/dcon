14d5723fb94b41d5c37780020106781743467f04
Corbin Simpson <MostAwesomeDude@gmail.com>
2012-06-19 12:04:49 -0700

    converters: Add datetime support and test.
    Refs #6.

--- newrem/converters.py
+++ newrem/converters.py
@@ -1,8 +1,20 @@
 from __future__ import with_statement
 
+from datetime import datetime
+from time import mktime
+
 from werkzeug.routing import BaseConverter, ValidationError
 
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.types import DateTime
+
+freezers = {
+    DateTime: lambda dt: str(int(mktime(dt.timetuple()))),
+}
+
+thawers = {
+    DateTime: lambda s: datetime.fromtimestamp(float(s)),
+}
 
 class ModelConverter(BaseConverter):
     """
@@ -12,11 +24,11 @@ class ModelConverter(BaseConverter):
     the `model` and `field` class attributes filled in. `model` is the
     Flask-SQLAlchemy model to use for queries, and `field` is the field on the
     model to use for lookups.
-
-    The field to use should be Unicode or bytes.
     """
 
     def to_python(self, value):
+        value = self.thawer(value)
+
         try:
             with self.app.test_request_context():
                 obj = self.model.query.filter_by(**{self.field: value}).one()
@@ -26,16 +38,20 @@ class ModelConverter(BaseConverter):
         return obj
 
     def to_url(self, value):
-        return getattr(value, self.field)
+        return self.freezer(getattr(value, self.field))
 
 def make_model_converter(a, m, f):
     """
     Create a ModelConverter for the given app, model, and field.
     """
 
+    column_type = type(m.__table__.columns[f].type)
+
     class Subclass(ModelConverter):
         app = a
         field = f
         model = m
+        freezer = staticmethod(freezers.get(column_type, str))
+        thawer = staticmethod(thawers.get(column_type, unicode))
 
     return Subclass
---
+++ newrem/test/test_converters.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from unittest import TestCase
+
+from flask import Flask
+
+from newrem.converters import make_model_converter
+from newrem.models import db, Newspost
+
+class TestNewspostConverter(TestCase):
+
+    def setUp(self):
+        self.app = Flask(__name__)
+        self.app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://"
+        self.converter_cls = make_model_converter(self.app, Newspost, "time")
+        self.converter = self.converter_cls(None)
+
+        db.init_app(self.app)
+        with self.app.test_request_context():
+            db.create_all()
+
+    def test_trivial(self):
+        pass
+
+    def test_apocalypse_to_url(self):
+        dt = datetime(2012, 12, 21)
+        news = Newspost("Test")
+        news.time = dt
+        expected = "1356076800"
+        self.assertEqual(self.converter.to_url(news), expected)
+
+    def test_apocalypse_to_python(self):
+        dt = datetime(2012, 12, 21)
+        news = Newspost(u"Test")
+        news.time = dt
+        # Hax: PID is needed by DB.
+        news.portrait_id = 0
+
+        fragment = "1356076800"
+
+        with self.app.test_request_context():
+            db.session.add(news)
+            db.session.commit()
+
+            result = self.converter.to_python(fragment)
+            db.session.add(result)
+
+            # No direct equality check, but this is close enough.
+            self.assertEqual(result.title, news.title)
+            self.assertEqual(result.time, news.time)
